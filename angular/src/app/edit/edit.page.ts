c4f55a012ef71b725a7938f6a431b8e4be1923a5
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-05-10 15:49:17 -0300

    3.c) Botão de editar os itens

--- angular/src/app/edit/edit.page.ts
+++ angular/src/app/edit/edit.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { legends } from '../names/nameslist';
 
 @Component({
   selector: 'app-edit',
@@ -6,11 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['edit.page.scss']
 })
 export class EditPage {
+  legends: string[][];
+  nameInput: string;
 
   constructor() {
     
   }
 
+  ngOnInit(){
+    // Criando um novo array com o  conteúdo do botão "linkado" com cada nome
+    this.legends = legends.map(value=> [value,'edit']);
+  }
+
+  onEdit(item) {
+    // Toggle de string
+    item[1] = (item[1]=='edit') ? 'save' : 'edit';
+
+    // Quando clicar Save e voltar para edit, o nome é salvo
+    if (item[1]==='edit'){
+      item[0] = this.nameInput;
+    }
+  }
+
+  onChange(e){
+    // O nome editado é salvo na variável nameInput
+    this.nameInput = e.target.value;
+  }
 }
 
 
